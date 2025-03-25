3ccf59b701004ce0919e46991fc249eecd94e02d
Risto Ikonen <ristoikonen@gmail.com>
2025-03-26 09:47:32 +1100

    Add project files.

---
+++ Data/IndexService.cs
@@ -0,0 +1,24 @@
+﻿using BlazorRazden.App.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorRazden.App.Data
+{
+    public class IndexService
+    {
+        // simulate HTTP GET from API
+        public Task<IndexModel[]> GetToDoItemAsync(DateTime startDate)
+        {
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new IndexModel
+            {
+                DateCreated = startDate.AddDays(index),
+                Description = (5000 + (index*6)).ToString() + " points.", 
+                ID = new Guid(),
+                IndexDate = startDate.AddDays(index).ToString("dddd"),
+                IndexValue = (5000 + (index * 6)),
+
+            }).ToArray());
+        }
+    }
+}
---
+++ Models/IndexModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlazorRazden.App.Models
+{
+    public class IndexModel
+    {
+        public Guid ID { get; set; }
+        public string? Description { get; set; }
+        public string? IndexDate { get; set; }
+        public double IndexValue { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
